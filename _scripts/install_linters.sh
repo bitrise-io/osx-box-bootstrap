ac133159f31f4fb7cb16846c4f49fbf6656b55a0
fehersanyi-bitrise <sandor.feher@bitrise.io>
2021-07-14 10:42:57 +0200

    add @ sign

--- _scripts/install_linters.sh
+++ _scripts/install_linters.sh
@@ -2,7 +2,7 @@
 set -e
 
 brew install ansible-lint@"${ANSIBLE_LINT_VER}"
-brew install yamllint"${YAML_LINT_VER}"
+brew install yamllint@"${YAML_LINT_VER}"
 
 yamllint --version
 ansible-lint --version
