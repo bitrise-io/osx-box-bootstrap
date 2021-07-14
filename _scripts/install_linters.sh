42d80b7a7f0860ddad72c1d11063a962b8dea668
fehersanyi-bitrise <sandor.feher@bitrise.io>
2021-07-14 10:42:39 +0200

    try with brew

--- _scripts/install_linters.sh
+++ _scripts/install_linters.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-export PATH=$PATH:/usr/local/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/bin
-pip3 install --user "ansible-lint==${ANSIBLE_LINT_VER}"
-pip3 install --user "yamllint==${YAML_LINT_VER}"
+brew install ansible-lint@"${ANSIBLE_LINT_VER}"
+brew install yamllint"${YAML_LINT_VER}"
 
 yamllint --version
 ansible-lint --version
