6406529f3a6ff20d72675124585656369cb0ef1b
fehersanyi-bitrise <sandor.feher@bitrise.io>
2021-07-14 10:23:24 +0200

    add bin path to path

--- _scripts/install_linters.sh
+++ _scripts/install_linters.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+
+export PATH=$PATH:/usr/local/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/bin
 pip3 install --user "ansible-lint==${ANSIBLE_LINT_VER}"
 pip3 install --user "yamllint==${YAML_LINT_VER}"
 
